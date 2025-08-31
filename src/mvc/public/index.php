804b64c81a7c547c32ec0906262464b91e64e5c4
nabab <thomas.nabet@gmail.com>
2025-09-01 01:48:47 +0200

    Fixed comments

--- src/mvc/public/index.php
+++ src/mvc/public/index.php
@@ -5,6 +5,6 @@
  * Time: 14:28
  */
 
-/** @var \bbn\Mvc\Controller $ctrl The current controller */
+/** @var bbn\Mvc\Controller $ctrl The current controller */
 
 $ctrl->combo(_("Terms and conditions for privacy protection"));
